52d2ebc39479c6e6e551552ebde4661ad47e20d2
RaquelLores <128092239+RaquelLores@users.noreply.github.com>
2024-02-08 13:44:21 +0100

    update form under construction

--- src/components/Update.jsx
+++ src/components/Update.jsx
@@ -3,9 +3,15 @@ import React from 'react'
 const Update = () => {
   return (
     <form action="">
-      <label htmlFor="">update</label>
+
+      <label htmlFor="especie">Especie:</label>
+      <input type="text" name="especie"/><br/>
+      <label htmlFor="trasplantado">Trasplantado:</label>
+      <input type="date" name="trasplantado"/><br/>
+      <label htmlFor="abonado">Abonado:</label>
+      <input type="date" name="abonado"/><br/>
     </form>
+    
   )
 }
-
 export default Update
