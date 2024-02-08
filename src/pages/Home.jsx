d9e5ebe1960074711a85535dfaa043501a4224af
Nho89 <noa1720@gmail.com>
2024-02-08 10:44:22 +0100

    modified outlet

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,16 +1,9 @@
 import React from 'react';
-import Nav from '../components/Nav';
-import Footer from '../components/Footer';
-
 
 const Home = () => {
   return (
     <>
-    
     <div>Mi página home</div>
-    <Nav/>
-    <Footer/>
-    
     </>
     
   )
--- src/router/router.jsx
+++ src/router/router.jsx
@@ -2,20 +2,28 @@ import { createBrowserRouter } from "react-router-dom";
 import Home from "../pages/Home";
 import Create from "../pages/Create";
 import Update from "../pages/Update";
+import LayoutPublic from "../components/LayoutPublic";
 
 const router = createBrowserRouter([
     {
       path: "/",
+      element: <LayoutPublic/>,
+      children:[
+        {
+          path: "/",
      element:  <Home/>,
-      
-    },
-    {
+        },
+        {
       path:"/create",
-      element: <Create/>
-    },
-    {
+      element: <Create/>,
+      },
+      {
       path: "/update",
-      element: <Update />
+      element: <Update />,
+      },
+    ],
     },
-  ]);
+    
+  ],
+  );
   export  default router;
