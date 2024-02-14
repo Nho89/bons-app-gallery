2d9db14b68298d4f4a58bcbf698f7e3fafa9bf58
Nho89 <noa1720@gmail.com>
2024-02-14 11:29:14 +0100

    modified folders

--- src/router/router.jsx
+++ src/router/router.jsx
@@ -1,7 +1,7 @@
 import { createBrowserRouter} from "react-router-dom";
-import Home from "../pages/Home";
-import Create from "../pages/Create";
-import Update from "../pages/Update";
+import Home from "../pages/home/Home";
+import Create from "../pages/create/Create";
+import Update from "../pages/update/Update";
 import LayoutPublic from "../components/LayoutPublic";
 import {deleteData, getData} from '../services/bonsaisServe'
 import CardDetail from "../components/CardDetail";
