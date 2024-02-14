219488e505c23983abec81a5103dee5c544d86ba
Nho89 <noa1720@gmail.com>
2024-02-14 23:40:42 +0100

    modifies styles

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div>
+    <div style={{position:"relative"}}>
       <hr />
       <p style={{textAlign: "center"}}>@Copyright 2024 BonsApp Gallery · Todos los derechos reservados</p>
     </div>
--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -1,42 +1,44 @@
 import React from 'react'
 import Delete from '../Delete'
 import Modify from '../Modify'
-import Modal from '../Modal'
+import Modal from '../modal/Modal'
+import { useState } from 'react'
 
 
 const Card = ({bonsais}) => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
   return (
    <>
    {
       bonsais.map(bonsai =>{
         return(
-      <div key={bonsai.id} className='Card-bons' style={{textAlign:"center", height:"80vh",width:"100%", display:"flex", alignItems:"center", justifyContent:"end", flexDirection:"column"}}>
+      <div key={bonsai.id} className='Card-bons' style={{textAlign:"center", /* height:"100vh", */width:"100%", display:"flex", alignItems:"center", justifyContent:"end", flexDirection:"column", gap:"20px"}}>
       
-        <div style={{width: "70%"}}><img src={bonsai.image} alt="" style={{maxWidth:"100%", textAlign:"center"}}/>
+        <div style={{width: "70%", display:"flex", flexWrap:"wrap", marginTop:"20%"}}><img src={bonsai.image} alt="" style={{maxWidth:"100%", textAlign:"center"}}/>
         </div>
       
-        <div style={{display: "flex", flexDirection:"column",alignItems:"center" ,justifyContent:"center",width:"98%",backgroundColor:"#044C40",  borderRadius:"20px", height:"400px"}}>
+        <div style={{display: "flex", flexDirection:"column",alignItems:"center" ,justifyContent:"center",width:"98%",backgroundColor:"#044C40",  borderRadius:"20px"/* , height:"400px" */}}>
         <h3 style={{color:"#F6F0BE", fontSize:"3em"}}>{bonsai.especie}</h3>
-        <div style={{display:"flex", gap:"80px", color:"#F6F0BE", fontSize:"1em"}}>
-        <p>{bonsai.trasplantado}</p>
-        <p>{bonsai.abonado}</p>  
+        <div style={{display:"flex", gap:"100px", color:"#F6F0BE", fontSize:"1em"}}>
+        <p>Trasplantado</p>
+        <p>Abonado</p>  
         </div>
         
         <div style={{display:"flex", gap:"20px", alignItems:"center"}} >
             <div style={{backgroundColor:"#F6F0BE",borderRadius:"20px", width:"180px"}}>
-                <p>Fecha</p>
+                <p>{bonsai.trasplantado}</p>
             </div>
             <div style={{backgroundColor:"#F6F0BE",borderRadius:"20px", width:"180px"}}>
-                <p>Fecha</p>
+                <p>{bonsai.abonado}</p>
             </div>
         </div>
-        <div style={{display: "flex", marginTop:"20px", marginBottom:"40px"}}>
-            <button>Detalles</button>
-        <Modal/>
+        <div style={{display: "flex", marginTop:"20px", marginBottom:"40px", gap:"10px"}}>
+            <button onClick={()=> setIsModalOpen(true)}>Detalles</button>
         <Modify/>
         <Delete id={bonsai.id}/>
         </div>
-        
+                <Modal isOpen = {isModalOpen} closeModal={()=> setIsModalOpen(false)}/>
+
         </div>
 
       </div>  
