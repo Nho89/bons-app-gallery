7149ddd4eb052344dd03da0fc717300079c2e8fd
DesireeCSilva <desiresilva1987@gmail.com>
2024-02-15 12:00:49 +0100

    changes on CSS of Create

--- src/pages/create/Create.jsx
+++ src/pages/create/Create.jsx
@@ -3,6 +3,7 @@ import {useForm} from  'react-hook-form'
 /* import {useHistory, useNavigate} from 'react-router-dom'
  */import axios from 'axios'
 import { postData } from '../../services/bonsaisServe'
+import './Create.css'
 
 const Create = () => {
       const { register, handleSubmit, reset } = useForm()
@@ -11,20 +12,22 @@ const Create = () => {
       }  
   return (
     <>
-    <form>
 
-        <label htmlFor="image">Foto:  </label><br />
-        <div >
-          <input type="file" style={{color: "transparent"}} id="image"  required/><br/>
+    <h1 className='title-bonsais'>Añadir Bonsai</h1>
+
+    <div >
+          <input type="file" style={{color: "transparent"}} id="image" required/><br/>
         </div>
+
+    <form className='container-form'>
         <label htmlFor="especie">Especie:  </label>
         <input type="text" name="especie" required/><br/>
         <label htmlFor="date-trasp">Trasplantado: </label>
         <input type="date" name="date-transp"/><br />
         <label htmlFor="date-abonado" required>Abonado: </label>
         <input type="date" /><br />
-        <button type='submit' >Enviar</button>
     </form>
+    <button type='submit' >Enviar</button>
     </>
   )
 }
