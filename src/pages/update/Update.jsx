a6096c792d7282330253b6c6cded86a78ca936fb
RaquelLores <128092239+RaquelLores@users.noreply.github.com>
2024-02-15 15:28:58 +0100

    update-method

--- src/pages/update/Update.jsx
+++ src/pages/update/Update.jsx
@@ -1,21 +1,32 @@
 import React from 'react'
-import Edit from '../../components/Edit'
-const Update = () => {
+//import 'css'
+import { useForm } from 'react-hook-form' //convierte info de un form en un objeto
+import { updateData } from '../../services/bonsaisServe'
+import { getData } from '../../services/bonsaisServe'
+
+const RegisterForm = async ({id}) => {
+const {id} = useParams();
+const { register, handleSubmit, formState: {errors} } = useForm()
+
+//Función que se ejecuta al enviar el formulario
+const onSubmit = (updatedbonsais) => {
+updateData updateBonsais;
+  };
   return (
     <>
-    <form action="">
-
+    <form onSubmit={handleSubmit(onSubmit)}>
       <label htmlFor="especie">Especie:</label>
-      <input type="text" name="especie"/><br/>
+      <input type="text" id="especie" {...register('especie',{required:'Este campo es obligatorio'})}/><br/>
       <label htmlFor="trasplantado">Trasplantado:</label>
-      <input type="date" name="trasplantado"/><br/>
+      <input type="date" id="trasplantado" {...register('trasplando',{required:'Este campo es obligatorio'})}/><br/>
       <label htmlFor="abonado">Abonado:</label>
-      <input type="date" name="abonado"/><br/>
-      <Edit/>
-    </form>
-
-    </>
-    
-  )
-}
-export default Update
+      <input type="date" id="abonado"{...register('abonado',{required:'Este campo es obligatorio'})}/><br/>
+{errors.nombre && <p>{errors.nombre.message}</p>}
+//botón de envío
+<button type="submit">Actualizar</button>
+</form>
+</>
+  );
+};
+  
+export default RegisterForm
