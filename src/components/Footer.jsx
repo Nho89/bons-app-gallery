501ee91a5929e002bd04ae469eae57d4ebb22e8c
DesireeCSilva <desiresilva1987@gmail.com>
2024-02-20 21:22:41 +0100

    last changes on CSS

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
     <div style={{position:"relative",  backgroundColor: "#044C40"}}>
       <hr style={{borderColor: "#F6F0BE", width:"70%"}} />
-      <p style={{textAlign: "center", color:"#F6F0BE"}}>@Copyright 2024 BonsApp Gallery · Todos los derechos reservados</p>
+      <p style={{textAlign: "center", color:"#F6F0BE", fontSize: "1rem"}}>@Copyright 2024 BonsApp Gallery · Todos los derechos reservados</p>
     </div>
   )
 }
