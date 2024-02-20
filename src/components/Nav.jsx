03f406dcef97eed8bbe988345be36a19a728d7d6
DesireeCSilva <desiresilva1987@gmail.com>
2024-02-20 20:23:01 +0100

    advance in media queries

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,12 +1,21 @@
 import React from 'react'
 import Add from './Add'
+import "../../src/index.css"
 
 const Nav = () => {
   return (
     <>
-    <nav className='navbar' style={{display: "flex", justifyContent: "space-between", padding:"20px"}} >
-      <div >Logo</div>
-           <Add/>
+    <nav className='navbar'>
+      <div>Logo</div>
+      
+      <div class="contain-menu">
+            <p class="header-text">Home</p>
+            <p class="header-text">Mis bonsáis</p>
+            <p class="header-text">Añadir</p>
+            <p class="header-text">Detalles</p>
+      </div>
+           
+      <Add/> 
     </nav>
     </>
     
