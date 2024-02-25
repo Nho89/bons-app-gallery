7d9f327d0be110982d41b03aadbc0d3ba1e92282
Nho89 <noa1720@gmail.com>
2024-02-25 23:23:52 +0100

    add link logo

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import Add from './Add'
+import { Link } from 'react-router-dom'
 
 const Nav = () => {
   return (
     <>
     <nav className='navbar' style={{display: "flex", justifyContent: "space-between", padding:"20px"}} >
-      <div >Logo</div>
-           <Add/>
+        <Link to="/" replace><img src="" alt="" />logo</Link>
+        <Add/> 
     </nav>
     </>
     
