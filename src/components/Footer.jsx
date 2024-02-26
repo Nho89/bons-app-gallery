3274a2723e97c638aac94ac876cedc4502a2f098
Nho89 <noa1720@gmail.com>
2024-02-26 11:32:41 +0100

    css

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div style={{position:"relative",  backgroundColor: "#044C40"}}>
-      <hr style={{borderColor: "#F6F0BE", width:"70%"}} />
-      <p style={{textAlign: "center", color:"#F6F0BE"}}>@Copyright 2024 BonsApp Gallery · Todos los derechos reservados</p>
+    <div style={{position:"relative",  backgroundColor: "#F6F0BE"}}>
+{      <hr style={{borderColor: "#000000", width:"70%", borderWidth:"thin" }} />
+}      <p style={{textAlign: "center", color:"#000000"}}>@Copyright 2024 BonsApp Gallery · Todos los derechos reservados</p>
     </div>
   )
 }
