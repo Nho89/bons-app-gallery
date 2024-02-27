b8602c93f1f16f1020c57fb69b0abffc82f668be
RaquelLores <128092239+RaquelLores@users.noreply.github.com>
2024-02-27 16:06:00 +0100

    testing components

---
+++ src/test/Nav.test.jsx
@@ -0,0 +1,18 @@
+import {render, screen} from "@testing-library/react";
+import Nav from "../components/Nav";
+import { expect } from "vitest";
+import { MemoryRouter } from "react-router";
+import { beforeEach } from "node:test";
+
+
+describe("NavTesting",() =>{
+
+    beforeEach(() =>{
+        render(<MemoryRouter><Nav/></MemoryRouter>);
+    })
+
+    test("when render Nav Añadir-button should appear",() => {
+        const buttonCardDetail = screen.getByText(/Mis bonsáis/i);
+        expect(buttonCardDetail).toBeDefined();
+    });
+});
