c63288909ec458d96014d12408711ae1ac103780
Nho89 <noa1720@gmail.com>
2024-02-27 21:02:41 +0100

    modified update cloudinary

--- src/pages/update/Update.jsx
+++ src/pages/update/Update.jsx
@@ -6,8 +6,8 @@ import { useParams, useNavigate } from 'react-router-dom';
 import axios from "axios";
 
 const Update = () => {
-  const [Url_Imagen, setUrl_Imagen ] = useState("");
   const [bonsai, setBonsai] = useState({}); 
+  const [Url_Imagen, setUrl_Imagen] = useState("");
   const { id } = useParams(); // obtenemos el parámetro id de la URL
   const navigate = useNavigate();
   const { register, handleSubmit, formState: { errors }, setValue } = useForm(); // con el hook useForm inicializamos un form con funciones y datos para gestionarlo.
@@ -25,7 +25,6 @@ const Update = () => {
         setValue('abonado', bonsaiData.abonado);
         setValue('trasplantado', bonsaiData.trasplantado);
         setValue('notas', bonsaiData.notas);
-        setValue('image', bonsaiData.image)
       } catch (error) {
         console.error('Error al obtener el bonsái:', error);
       }
@@ -35,8 +34,7 @@ const Update = () => {
 
   //cuando enviamos el form, se ejecuta la función onSubmit, que llama a updateData, para actualizar datos e ir luego a la home.
   const onSubmit = async (newData) => {
-    console.log(newData)
-    newData.image = Url_Imagen
+    newData.image = Url_Imagen;
     try {
       await updateData(id, newData);
       console.log('Datos actualizados correctamente');
@@ -47,30 +45,29 @@ const Update = () => {
   };
 //gestionamos el envío del form con handleSubmit. Se registran los campos en register.
   const changeUploadImage = async (e) => {
-    const file = e.target.files[0];
+  const file = e.target.files[0];
+  const newData = new FormData();
+  newData.append("file", file);
+  newData.append("upload_preset", "preset_bonsai"); // Asegúrate de usar el preset correcto
 
-    const data = new FormData(); 
-
-    data.append("file", file);
-    data.append("upload_preset","preset_bonsai");
-
-
+  try {
     const response = await axios.put(
-      "https://api.cloudinary.com/v1_1/dputvv9bi/image/upload", 
-      data
-      );
-      setUrl_Imagen(response.data.secure_url);
-      console.log(response.data);
+      "https://api.cloudinary.com/v1_1/dputvv9bi/image/upload",
+      newData
+    );
+    setUrl_Imagen(response.data.secure_url);
+    console.log(response.data);
+  } catch (error) {
+    console.error("Error al cargar la imagen a Cloudinary:", error);
+  }
 };
   return (
     <>
       <form className='container-form' onSubmit={handleSubmit(onSubmit)}> 
         <h1 className='title-bonsais'>Modificar mi bonsái</h1>
         {/* Campos del formulario */}
-        <label htmlFor="image">Cambia la imagen de tu Bonsai<img src="https://res.cloudinary.com/dvko0roau/image/upload/v1708026581/add_frame_tbf87i.png" alt="imagen de un marco de fotos" /></label>
-        <input id="image" style={{ color: "transparent", opacity: 0, position: "absolute" }} type="file" 
-        {...register("image", { required: true })} accept="image/*" onChange={changeUploadImage} />
-
+        <label htmlFor="image" >Cambia la imagen de tu Bonsai<img  src="https://res.cloudinary.com/dvko0roau/image/upload/v1708026581/add_frame_tbf87i.png" alt="imagen de un marco de fotos" /></label>
+        <input id="image" type="file" style={{ color: "transparent", opacity: 0, position: "absolute" }}   onChange={changeUploadImage}/>
         {Url_Imagen && (
         <div>
         <img src={Url_Imagen}  alt="Imagen de mi bonsai" style={{maxWidth:"200px"}}/>
@@ -93,7 +90,7 @@ const Update = () => {
         <textarea className='label-form-notas' id='notas' {...register('notas', { required: true })} type='text' name='notas' placeholder='' />
         { errors.notas && <span className='span-error'> La casilla de notas es requerida </span>}
         {/* Botón de envío */}
-        <button className='boton-form-update' type='submit' >Enviar</button>
+        <button className='boton-form-update' type='submit'>Enviar</button>
       </form>
     </>
   );
