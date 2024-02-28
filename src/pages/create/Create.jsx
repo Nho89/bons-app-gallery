802d8e4005b62e5271431270195f9f175fc2c288
Nho89 <noa1720@gmail.com>
2024-02-28 21:52:30 +0100

    modified validations create

--- src/pages/create/Create.jsx
+++ src/pages/create/Create.jsx
@@ -8,7 +8,7 @@ import './Create.css'
 const Create = () => {
   const [Url_Imagen, setUrl_Imagen ] = useState("");
   const navigate = useNavigate()
-  const { handleSubmit,register, errors} = useForm()
+  const { handleSubmit,register, formState: { errors }} = useForm()
 
 const onSubmit = (data) => {
   data.image = Url_Imagen
@@ -58,16 +58,28 @@ const changeUploadImage = async (e) => {
         
         <div className='form-right-create'>
          <label htmlFor="especie">Especie:</label>
-        <input className="label-form-create" type='text' {...register("especie", { required: true })} required/>
+        <input className="label-form-create" type='text' {...register("especie", { required: 'La especie es requerida' })}/>
+        {errors.especie && <span className='span-error'>{errors.especie.message}</span>}
+
+        
         <div className='dates-input-create'>
         <label htmlFor="trasplantado">Trasplantado</label>
-        <input className="label-form-create" id='trasplantado' type='date' {...register("trasplantado", { required: true })} required />
-       
+        <input className="label-form-create" id='trasplantado' type='date' {...register("trasplantado", { required: true })} />
+        <br />
+        {errors.trasplantado && <span  className='span-error'> La fecha de trasplantado es requerida </span>}
+        <br />
+
+
         <label  htmlFor="abonado">Abonado</label>
-        <input type='date' className="label-form-create" name="abonado" id="abonado" {...register("abonado")} required/>
+        <input type='date' className="label-form-create" name="abonado" id="abonado" {...register("abonado", { required: true })} />
+        <br />
+        {errors.abonado && <span className='span-error'> La fecha de abonado es requerida </span>}
+
         </div>
         <label htmlFor="notas">Descripción</label>
-        <textarea className="label-form-notas" type="text" name="notas" id="notas" {...register("notas")} placeholder=""/>
+        <textarea className="label-form-notas" type="text" name="notas" id="notas" {...register("notas", { required: true })} placeholder=""/> <br />
+        { errors.notas && <span className='span-error'> La casilla de notas es requerida </span>}
+
 
         <button style={{width: "25vw", backgroundColor:"#F69E92", color: "#000000", padding:"6px", borderRadius:"10px", fontFamily: 'Poppins', fontSize: "1rem", border: "none", boxShadow: "0 2px 4px rgba(0,0,0,0.2)", width: "90%", marginBottom: "10%"  }} className="boton-form" type='submit'>Enviar</button> 
         </div>
